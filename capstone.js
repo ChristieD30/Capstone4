6d8e5c54a662b33f371f8cb2224aa062ca2f0026
Christie <Christie.Desnoyer@gmail.com>
2018-11-06 21:05:39 -0500

    Stylized website

--- capstone.js
+++ capstone.js
@@ -3,9 +3,9 @@ var itemNames = new Array();
   var shopCart = new Array ();
 function addToCart(itemName, itemPrice){
  //step 1 find the element
-  var cart = document.getElementById("shoppingcart");
+  // var cart = document.getElementById("shoppingcart");
  
-  cart.innerHTML +=  "<p>" + itemName + " " + itemPrice + "</p>";
+ //cart.innerHTML +=  "<p>" + itemName + ", $ " + itemPrice + "</p>";
 
 
 itemNames.push(itemName); 
@@ -18,11 +18,22 @@ itemPrices.push(itemPrice);
 
 function disp(){
 var result="";
-
+var sum = 0.0;
 for (var i = 0; i < itemNames.length; i++) {     
-     result += (itemNames[i] + " : $" + itemPrices[i]); 
+     result += (itemNames[i] + " : $" + itemPrices[i]);
+     
 
- 
 }
-return console.log(result);
+
+for(var i=0; i < itemPrices.length; i++){
+
+    sum += itemPrices[i];
+
 }
+
+tax = sum * 1.06;
+
+alert("Your items: " + result + " Total: $" + sum.toFixed(2) + "With tax: $"+ tax.toFixed(2));
+
+}
+
